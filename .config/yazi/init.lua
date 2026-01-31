17fb306d630f6409196891b12a963e7b388891a3
Stanislav Dzisiak <stanislav.dzisiak@gmail.com>
2026-01-31 12:18:08 +0200

    update yazi pugins and add git plugin

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -6,7 +6,7 @@ require("full-border"):setup()
 -- https://yazi-rs.github.io/docs/tips/#user-group-in-status
 Status:children_add(function()
   local h = cx.active.current.hovered
-  if h == nil or ya.target_family() ~= "unix" then
+  if not h or ya.target_family() ~= "unix" then
     return ""
   end
 
@@ -20,16 +20,18 @@ end, 500, Status.RIGHT)
 
 -- Show symlink in status bar
 -- https://yazi-rs.github.io/docs/tips/#symlink-in-status
-function Status:name()
+Status:children_add(function(self)
   local h = self._current.hovered
-  if not h then
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
     return ""
   end
+end, 3300, Status.LEFT)
 
-  -- return " " .. h.name:gsub("\r", "?", 1)
-  local linked = ""
-  if h.link_to ~= nil then
-    linked = " -> " .. tostring(h.link_to)
-  end
-  return ui.Line(" " .. h.name .. linked)
-end
+-- Show the status of Git file changes as linemode in the file list
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi
+require("git"):setup {
+  -- Order of status signs showing in the linemode
+  order = 1500,
+}
